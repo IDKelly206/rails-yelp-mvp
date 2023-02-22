3781457606e94445118a9f186b1835149db8f98e
Ian Kelly <idk13@hotmail.com>
2023-02-22 14:34:52 +0100

    Restuarant model migrated and db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Clearing DB"
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+15.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  puts "Restaurant with id: #{restaurant.id} created"
+end
+
+puts "Finished seed DB"
